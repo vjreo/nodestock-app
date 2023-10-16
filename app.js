48114c338de6d78b22e744d6ad63ec8cf4005427
Vincenzo Reo <vincenzojreo@gmail.com>
2023-10-16 13:30:44 -0400

    add api and routes for Stocky app - node practice

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const app = express();
-
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 4000;
+
+// Use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
-// Set Handlebar Routes
+// create call_api function
+function call_api(finishedAPI, ticker) {
+    request(`https://api.iex.cloud/stable/stock/${ticker}/quote?token=pk_48a58fd0bd0e4c25aa30bfe2b47e8e20`, { json: true }, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        } else if (res.statusCode === 200) {
+            finishedAPI(body);
+        };
+    });
+};
+
+// Set Handlebar app GET route
 app.get('/', (req, res) => {
-    res.render('home');
+    //callback function to allow api call to finish
+    call_api(function(doneAPI) {
+            res.render('stock-track', {
+            stock: doneAPI
+        });
+    });
+});
+
+// Set Handlebar app POST route
+app.post('/', (req, res) => {
+    //callback function to allow api call to finish
+    call_api(function(doneAPI) {
+            res.render('stock-track', {
+            stock: doneAPI,
+        });
+    }, req.body.stock_ticker);
+});
+
+// create homepage route
+app.get('/', function (req, res){
+        res.render('index.html');
 });
 
 // Set static folder
